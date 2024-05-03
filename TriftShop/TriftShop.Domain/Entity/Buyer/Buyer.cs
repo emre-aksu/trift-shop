7db232f1fc9fa726b2c6ce9dad2d8be71b6f339e
Emre Aksu <emreaksu92@outlook.com>
2024-05-03 20:16:54 +0300

    Add buyer and buyer address entity.

---
+++ TriftShop/TriftShop.Domain/Entity/Buyer/Buyer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriftShop.Domain.Entity.Buyer
+{
+    public class Buyer
+    {
+        public int Id { get; set; }
+        public string IdentityNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber {  get; set; }    
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isEmailVerified { get; set; }
+        public bool isPhoneNumberVerified { get; set; }
+
+        public List<BuyerAddress> BuyerAddresses {  get; set; }
+
+    }
+}
---
+++ TriftShop/TriftShop.Domain/Entity/Buyer/BuyerAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriftShop.Domain.Entity.Buyer
+{
+    public class BuyerAddress
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AddresLine { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Neighborhood { get; set; }
+        public string Street { get; set; }
+        public string FlatNumber { get; set; }
+        public string Floor { get; set; }
+        
+        public Buyer Buyer { get; set; }
+    }
+}
